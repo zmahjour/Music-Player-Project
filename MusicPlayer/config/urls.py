1d857fd7a45986040443b28f2e80dd71fe541704
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-02 23:31:58 +0330

    chore: add static and media url to urlpatterns

--- MusicPlayer/config/urls.py
+++ MusicPlayer/config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -23,3 +25,5 @@ urlpatterns = [
     path("", include("interactions.urls", namespace="interactions")),
     path("", include("songs.urls", namespace="songs")),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
