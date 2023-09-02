bcd5e24ba98ad66068d639f178c071bd63409eed
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-02 23:30:51 +0330

    chore: include apps urls and define namespace for each app

--- MusicPlayer/config/urls.py
+++ MusicPlayer/config/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("accounts.urls", namespace="accounts")),
+    path("", include("interactions.urls", namespace="interactions")),
+    path("", include("songs.urls", namespace="songs")),
 ]
