f12c641987e76162cdb60056fd0d3df66b719ccb
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-04 12:19:19 +0330

    chore: define BaseUser model that inherits from AbstractBaseUser and PermissionsMixin

--- MusicPlayer/accounts/models.py
+++ MusicPlayer/accounts/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+from .managers import CustomUserManager
+
+
+class BaseUser(AbstractBaseUser, PermissionsMixin):
+    class Gender(models.TextChoices):
+        MALE = "Male"
+        FEMALE = "Female"
+        OTHER = "Other"
+
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=50)
+    bio = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to="users", null=True, blank=True)
+    country = models.CharField(max_length=50, null=True, blank=True)
+    city = models.CharField(max_length=50, null=True, blank=True)
+    joined = models.DateTimeField(auto_now_add=True)
+    gender = models.CharField(
+        max_length=6, choices=Gender.choices, null=True, blank=True
+    )
+    birthday = models.DateField(null=True, blank=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "name"]
+
+    def __str__(self):
+        return self.username
+
 
 # Create your models here.
