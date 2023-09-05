ba84f1a3cd3adc4ad1ec06fca30182d488038d05
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-05 23:29:07 +0330

    chore: define Like model

--- MusicPlayer/interactions/models.py
+++ MusicPlayer/interactions/models.py
@@ -3,3 +3,11 @@ from django.db import models
 from accounts.models import Listener
 from songs.models import Song
 
+
+class Like(models.Model):
+    user = models.ForeignKey(Listener, on_delete=models.CASCADE)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user} liked "{self.song}"'
+
