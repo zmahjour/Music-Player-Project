c63cd4ba613c13c70eef788029b9c52a54645ac1
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-06 21:56:04 +0330

    chore: make custom model admin classes and register accounts app models

--- MusicPlayer/accounts/admin.py
+++ MusicPlayer/accounts/admin.py
@@ -1,3 +1,89 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Listener, Artist, BaseUser, Band
+
+
+@admin.register(BaseUser)
+class BaseUserAdmin(UserAdmin):
+    list_display = ["username", "email", "is_staff"]
+    list_filter = ["is_staff", "country", "gender"]
+    fieldsets = [
+        (None, {"fields": ["username", "email", "password"]}),
+        (
+            "Personal info",
+            {
+                "fields": [
+                    "name",
+                    "bio",
+                    "image",
+                    "country",
+                    "city",
+                    "gender",
+                    "birthday",
+                ]
+            },
+        ),
+        ("Permissions", {"fields": ["is_active", "is_staff"]}),
+    ]
+
+    add_fieldsets = [
+        (
+            None,
+            {
+                "fields": ["username", "email", "name", "password1", "password2"],
+            },
+        ),
+    ]
+    search_fields = ["username", "email"]
+    ordering = ["username"]
+    filter_horizontal = []
+
+
+@admin.register(Listener)
+class ListenerAdmin(BaseUserAdmin):
+    list_filter = ["is_staff", "country", "gender", "account_type"]
+    fieldsets = [
+        (None, {"fields": ["username", "email", "password"]}),
+        (
+            "Personal info",
+            {
+                "fields": [
+                    "name",
+                    "bio",
+                    "image",
+                    "country",
+                    "city",
+                    "gender",
+                    "birthday",
+                ]
+            },
+        ),
+        ("Permissions", {"fields": ["account_type", "is_active", "is_staff"]}),
+    ]
+
+
+@admin.register(Artist)
+class ArtistAdmin(BaseUserAdmin):
+    fieldsets = [
+        (None, {"fields": ["username", "email", "password"]}),
+        (
+            "Personal info",
+            {
+                "fields": [
+                    "name",
+                    "bio",
+                    "image",
+                    "country",
+                    "city",
+                    "gender",
+                    "birthday",
+                    "band",
+                ]
+            },
+        ),
+        ("Permissions", {"fields": ["is_active", "is_staff"]}),
+    ]
+
+
+admin.site.register(Band)
