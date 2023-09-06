5d617215faeb2f65a95e2f1b60f3fbce28dea87e
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-06 21:54:15 +0330

    chore: define login, register and logout urls

--- MusicPlayer/accounts/urls.py
+++ MusicPlayer/accounts/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
 from . import views
 
 app_name = "accounts"
-urlpatterns = []
+urlpatterns = [
+    path("login/", views.UserLoginView.as_view(), name="user_login"),
+    path("register/", views.UserRegisterView.as_view(), name="user_register"),
+    path("logout/", views.UserLogoutView.as_view(), name="user_logout"),
+]
